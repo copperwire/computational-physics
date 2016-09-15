a075278bb4bf95de72a78bf4c16bc2f7c2675e44
copperwire <solli.robert@gmail.com>
2016-09-15 11:20:07 +0200

    special algo functioning

--- project1/build-project_1-Desktop_Qt_5_7_0_GCC_64bit-Default/plot_exact.py
+++ project1/build-project_1-Desktop_Qt_5_7_0_GCC_64bit-Default/plot_exact.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 U = np.loadtxt("forw_back.txt")
+U_other = np.loadtxt("special_result.txt")
 U = U[::-1]
+U_other = U_other[::-1]
 N = np.size(U)
 
 exact = lambda x: 1-(1-np.exp(-10))*x - np.exp(-10*x)
@@ -15,6 +18,7 @@ u[1:N-1] = exact(x)
 
 
 plt.plot(x, u[1:N-1], "r-", label = "Exact solution")
-plt.plot(X, U, "b-x", label = "Forw + back subst")
+#plt.plot(X, U, "b-x", label = "Forw + back subst")
+plt.plot(X, U_other, "g-x", label = "spec algo ")
 plt.legend()
 plt.show()
--- project1/project_1/main.cpp
+++ project1/project_1/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include <math.h>
 #include <armadillo>
 #include <stdlib.h>
-
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    int N = atoi(argv[1]);
+void non_special_method(double U [], int N){
+    char filename []  = "forw_back.txt";
     double h = 1./(N+1);
+    double hh = h*h;
 
     //A = upper diagonal
     //B = lower diagonal
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     double* B = new double[N+2];
     double* M = new double[N+2];
     double* F = new double[N+2];
-    double* U = new double[N+2];
+
 
 
     /* Remember to write to file
@@ -29,23 +29,82 @@ int main(int argc, char *argv[])
      * The function needs to be EXACTLY 0 at boundary conditions DIRICHLET MOTHERFUCKER
     c*/
 
+    ofstream datafile (filename);
+
     for(int i = 0; i<N+2; i++){
         A[i] = -1.0;
         B[i] = -1.0;
         M[i] = 2.0;
-        F[i] = h*h*100*exp(-10* i*h);
+        F[i] = hh*100*exp(-10* i*h);
 
     }
+
+    // Equations are fetched from the lecture notes on linear algebra regard. a tri-diagonal matrix on a general form.
 
     for(int i = 2; i < N +2; i++){
-        M[i] = M[i] - (A[i] * B[i-1])/M[i-1];
-        F[i] = F[i] - (A[i] * F[i-1])/M[i-1];
+        M[i] = M[i] - (B[i] * A[i-1])/M[i-1];
+        F[i] = F[i] - (B[i] * F[i-1])/M[i-1];
     }
 
-    for(int i = N; i > 1; i--){
-        U[i - 1] = (F[i-1] -  C[i-1] * U[i])/B[i-1] ;
+    // writing outside loop to include boundary conditions
+    datafile << U[0] << endl;
+
+    for(int i = N + 1; i > 1; i--){
+        U[i - 1] = (F[i-1] -  A[i-1] * U[i])/M[i-1] ;
+        datafile << U[i -1] << endl ;
     }
 
+    datafile << U[N+2] ;
+}
 
+
+void special_method(double U [], int N){
+    char filename []  = "special_result.txt";
+    double h = 1./(N+1);
+    double hh = h*h;
+
+    //M = midle diagonal
+    //F = function evaluation
+    //U = unknown function result
+
+    double* M = new double[N+2];
+    double* F = new double[N+2];
+
+    M[1] = M[N] =  2.0;
+    F[1] = hh*100*exp(-10 * h);
+
+    for(int i = 2; i < N+2 ; i++){
+        M[i] = (i + 1.0)/((double) i);
+        F[i] = hh*100*exp(-10* i*h) + F[i-1]/M[i-1] ;
+    }
+
+     ofstream datafile (filename);
+
+     datafile << U[N+1] << endl;
+
+     U[N] = F[N]/M[N];
+
+     datafile << U[N] << endl;
+
+     for(int i = N-1 ; i > 0; i--){
+         U[i] = (F[i] + U[i+1])/M[i];
+         datafile << U[i] << endl ;
+     }
+
+     datafile << U[0] ;
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    int N = atoi(argv[1]);
+    double* U = new double[N+2];
+    double* U_spec = new double[N+2];
+
+    non_special_method(U, N);
+    special_method(U_spec, N);
+
+    return 0;
 
 }
