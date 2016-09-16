420d54a272b89c0e1f141360a8e1c05bfc73c3bd
copperwire <solli.robert@gmail.com>
2016-09-16 18:00:57 +0200

    working again

--- project1/build-project_1-Desktop_Qt_5_7_0_GCC_64bit-Default/plot_exact.py
+++ project1/build-project_1-Desktop_Qt_5_7_0_GCC_64bit-Default/plot_exact.py
@@ -2,23 +2,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-U = np.loadtxt("forw_back.txt")
-U_other = np.loadtxt("special_result.txt")
-U = U[::-1]
-U_other = U_other[::-1]
-N = np.size(U)
+for i in range(5):
+	try: 
+		A_gen = np.loadtxt("forw_back" + "{}".format(i))
+		A_spec = np.loadtxt("special_result"+ "{}".format(i))
+		
+		x_spec = A_spec[:,0]
+		U_spec = A_spec[:,1]
+		exact_spec = A_spec[:,2]
+		eps_spec = A_spec[:,3]
 
-exact = lambda x: 1-(1-np.exp(-10))*x - np.exp(-10*x)
+		x_gen = A_spec[:,0]
+		U_gen = A_spec[:,1]
+		exact_gen = A_spec[:,2]
+		eps_gen = A_spec[:,3]
 
-x = np.linspace(0.001, 0.999, N - 2)
-X = np.linspace(0, 1, N)
-u = np.zeros(N)
+		Np1 = len(x_gen)
+		
+		plt.plot(x_gen, exact_gen, "r-", label = "Exact solution")
+		plt.plot(x_gen, U_gen, "b-", label = "gen algor.")
+		plt.plot(x_spec, U_spec, "g-", label = "spec algor.")
+		plt.legend()
+		plt.title("Exact vs numerical algorithms")
+		plt.ylabel(r"$y_i$")
+		plt.xlabel(r"$x_i$")
+		plt.savefig("figure_"+"{}".format(i))
+		plt.cla()
+		plt.clf()
 
-u[1:N-1] = exact(x)
+		plt.plot(x_gen[1:Np1-1], eps_gen[1:Np1-1], label = "General algorithm")
+		plt.plot(x_spec[1:Np1-1], eps_spec[1:Np1-1], label = "Special algorithm")
+		plt.legend()
+		plt.title(r"Relative error, $\epsilon_r$, comparison")
+		plt.ylabel(r"$\epsilon_r$")
+		plt.xlabel(r"$x_i$")
+		plt.savefig("error_plot_{}".format(i))
+		plt.cla()
+		plt.clf()
 
-
-plt.plot(x, u[1:N-1], "r-", label = "Exact solution")
-plt.plot(X, U, "b-x", label = "Forw + back subst")
-plt.plot(X, U_other, "g-x", label = "spec algo ")
-plt.legend()
-plt.show()
+		"latex writing code should go here"
+	except FileNotFoundError:
+		continue
--- project1/project_1/main.cpp
+++ project1/project_1/main.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string>
 #include <iomanip>
+#include <time.h>
+#include <armadillo>
 
 using namespace std;
 
@@ -14,117 +16,151 @@ double exact(double x){
     return 1.0-(1-exp(-10))*x-exp(-10*x);
 }
 
+int non_special_method(int exponent){
 
-void non_special_method(double U [], int exponent){
-    char filename []  = "forw_back.txt";
-    int N = pow(10, exponent);
-    double h = 1./(N+1);
-    double hh = h*h;
+    string time_file = "general_time";
+    ofstream time_write (time_file.c_str());
+    time_write << "#num iterations, seconds/clocks_per_sec" <<endl;
 
-    //A = upper diagonal
-    //B = lower diagonal
-    //M = midle diagonal
-    //F = function evaluation
-    //U = unknown function result
-
-    double* A = new double[N+2];
-    double* B = new double[N+2];
-    double* M = new double[N+2];
-    double* F = new double[N+2];
-
-
-
-    /* Remember to write to file
-     * Implement the exact function in python - faster
-     * NB!
-     * The function needs to be EXACTLY 0 at boundary conditions DIRICHLET MOTHERFUCKER
-    c*/
-
-    ofstream datafile (filename);
-
-    for(int i = 0; i<N+2; i++){
-        A[i] = -1.0;
-        B[i] = -1.0;
-        M[i] = 2.0;
-        F[i] = hh*100*exp(-10* i*h);
-
-    }
-
-    // Equations are fetched from the lecture notes on linear algebra regard. a tri-diagonal matrix on a general form.
-
-    for(int i = 2; i < N +2; i++){
-        M[i] = M[i] - (B[i] * A[i-1])/M[i-1];
-        F[i] = F[i] - (B[i] * F[i-1])/M[i-1];
-    }
-
-    // writing outside loop to include boundary conditions
-    datafile << U[0] << endl;
-
-    for(int i = N + 1; i > 1; i--){
-        U[i - 1] = (F[i-1] -  A[i-1] * U[i])/M[i-1] ;
-        datafile << U[i -1] << endl ;
-    }
-
-    datafile << U[N+2] ;
-}
-
-
-void special_method(double U [], int exponent){
     for(int i = 1; i <= exponent; i++){
-        string filename  = "special_result";
-        string rel_err_file = "rel_err";
 
-        string new_string = filename + rel_err_file;
-
+        string filename = "general_results";
         string argument = to_string(i);
         filename.append(argument);
-        rel_err_file.append(argument);
+        filename.append(".txt");
 
         int N = pow(10, i);
         double h = 1./(N+1);
         double hh = h*h;
+        double start, stop;
+
+        //A = upper diagonal
+        //B = lower diagonal
+        //M = midle diagonal
+        //F = function evaluation
+        //U = unknown function result
+
+        double* A = new double[N+2];
+        double* B = new double[N+2];
+        double* M = new double[N+2];
+        double* F = new double[N+2];
+        double* U = new double[N+2];
+        double* x = new double[N+2];
+
+        /* Remember to write to file
+         * Implement the exact function in python - faster
+         * NB!
+         * The function needs to be EXACTLY 0 at boundary conditions DIRICHLET MOTHERFUCKER
+        */
+
+        x[1] = U[1] = F[1] = 0; x[N+1] = U[N+1] = F[N+1] = 0;
+
+        for (int i = 1; i<N+2; i++){
+            x[i] = i*h;
+            F[i] = hh*100*exp(-10* x[i]);
+        }
+        for(int i = 0; i<N+2; i++){
+            A[i] = -1.0;
+            B[i] = -1.0;
+            M[i] = 2.0;
+        }
+
+        // Start of algoritm
+
+        start = clock();
+        for(int i = 2; i < N +2; i++){
+            M[i] = M[i] - (B[i] * A[i-1])/M[i-1];
+            F[i] = F[i] - (B[i] * F[i-1])/M[i-1];
+        }
+
+        U[N] = F[N]/M[N];
+
+        for(int i = N -1 ; i > 1; i--){
+            U[i] = (F[i] -  A[i] * U[i +1])/M[i] ;
+        }
+        stop = clock ();
+        // end of algorithm
+
+        time_write << N <<"  "<<(double (stop - start) / CLOCKS_PER_SEC) << endl ;
+
+        ofstream rel_file (filename.c_str());
+
+        for(int i = 1; i < N + 2; i++){
+           double exact_val  = exact(x[i]);
+           double Relerr = fabs( (exact_val - U[i]) / exact_val);
+
+           rel_file << setw(15) << setprecision(8) << x[i];
+           rel_file << setw(15) << setprecision(8) << U[i];
+           rel_file << setw(15) << setprecision(8) << exact_val;
+           rel_file << setw(15) << setprecision(8) << log10(Relerr) << endl;
+           }
+
+
+
+        rel_file.close();
+        delete A; delete B; delete M; delete F; delete U; delete x;
+        }
+    return 0;
+    time_write.close();
+}
+
+
+int special_method(int exponent){
+    string time_file = "special_time";
+    ofstream time_write (time_file.c_str());
+    time_write << "#num iterations, seconds/clocks_per_sec" <<endl;
+
+    for(int i = 1; i <= exponent; i++){
+
+        string rel_err_file = "special_result";
+
+        string argument = to_string(i);
+        rel_err_file.append(argument);
+        rel_err_file.append(".txt");
+
+        int N = pow(10, i);
+        double h = 1./(N+1);
+        double hh = h*h;
+        double start, stop;
 
         //M = midle diagonal
         //F = function evaluation
         //U = unknown function result
 
         double* M = new double[N+2];
+        double* U = new double[N+2];
         double* F = new double[N+2];
-        double* x = new double[N+2];
+        double* x = new double[N+2]; 
 
-
+        x[1] = U[1] = F[1] = 0; x[N+1] = U[N+1] = F[N+1] = 0;
         M[1] = M[N] =  2.0;
-        F[1] = hh*100*exp(-10 * h);
 
         for (int i = 2; i< N+2; i++){
             x[i] = i*h;
+            M[i] = (i + 1.0)/((double) i);
+            F[i] = hh*funct(x[i]);
         }
 
+        //start of algorithm
+        start = clock();
 
         for(int i = 2; i < N+2 ; i++){
-            M[i] = (i + 1.0)/((double) i);
-            F[i] = hh*funct(x[i]) + F[i-1]/M[i-1] ;
+            F[i] = F[i] + F[i-1]/M[i-1] ;
         }
-
-         ofstream datafile (filename.c_str());
-
-         datafile << U[N+1] << endl;
 
          U[N] = F[N]/M[N];
 
-         datafile << U[N] << endl;
-
-         for(int i = N-1 ; i > 0; i--){
+         for(int i = N-1 ; i > 1; i--){
              U[i] = (F[i] + U[i+1])/M[i];
-             datafile << U[i] << endl ;
          }
-
-         datafile << U[0] ;
-
+         stop = clock();
+         //end of algorithm
+         time_write << N <<"  "<<(double (stop - start) / CLOCKS_PER_SEC) << endl ;
 
          ofstream rel_file (rel_err_file.c_str());
 
-         for(int i = 2; i < N+2; i++){
+
+         for(int i = 1; i < N +2; i++){
             double exact_val  = exact(x[i]);
             double Relerr = fabs( (exact_val - U[i]) / exact_val);
 
@@ -132,22 +168,23 @@ void special_method(double U [], int exponent){
             rel_file << setw(15) << setprecision(8) << U[i];
             rel_file << setw(15) << setprecision(8) << exact_val;
             rel_file << setw(15) << setprecision(8) << log10(Relerr) << endl;
-         }
+            }
 
-
+         rel_file.close();
+         delete M; delete F; delete x; delete U;
     }
-
+time_write.close();
+return 0;
 }
 
 
-int main(int argc, char *argv[])
-{
-    int N = atoi(argv[1]);
-    double* U = new double[N+2];
-    double* U_spec = new double[N+2];
+int main(int argc, char *argv[]){
+    int exponent = atoi(argv[1]);
+    int N = pow(10, exponent);
 
-    non_special_method(U, N);
-    special_method(U_spec, N);
+    double* U = new double[N+2];
+    non_special_method(exponent);
+    special_method(exponent);
 
     return 0;
 
